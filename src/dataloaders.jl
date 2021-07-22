7458baa32195de8d599950740453d791d7e9f365
Athul Sudheesh <athuls0708@gmail.com>
2021-07-22 10:52:17 -0500

    Update dataloaders.jl

--- src/dataloaders.jl
+++ src/dataloaders.jl
@@ -1,12 +1,12 @@
 
 function load_data(dataset_name)
     rda = dataset_name * ".rda"
-    basename = joinpath(dirname(dirname(pathof(CDMrdata))),"data")
+    basename = joinpath(dirname(dirname(pathof(CDMrdata))),"..","data")
     try 
         data = load(joinpath(basename,rda))[ "data." * dataset_name]
         return data 
     catch
-        data = load(joinpath("data",rda))[dataset_name]
+        data = load(joinpath(basename,rda))[dataset_name]
         return data 
     end
 end
