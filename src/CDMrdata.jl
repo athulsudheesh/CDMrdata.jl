ee72bbed464dedd0ef8e9670316a0494ebb1b01b
Athul Sudheesh <athuls0708@gmail.com>
2021-07-22 10:59:12 -0500

    Update CDMrdata.jl

--- src/CDMrdata.jl
+++ src/CDMrdata.jl
@@ -1,7 +1,5 @@
 module CDMrdata
-    if isdefined(Base, :Experimental) && isdefined(Base.Experimental, Symbol("@optlevel"))
-        @eval Base.Experimental.@optlevel 1
-    end
+
     using CodecXz
     using CodecBzip2    
     using RData
