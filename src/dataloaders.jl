7808376f720137330ec3963c116933fb6684c5fa
Athul Sudheesh <athuls0708@gmail.com>
2021-07-22 09:59:45 -0500

    testing directory path for load_data

--- src/dataloaders.jl
+++ src/dataloaders.jl
@@ -1,8 +1,9 @@
 
 function load_data(dataset_name)
     rda = dataset_name * ".rda"
+    basename = joinpath(@__DIR__, "..")
     try 
-        data = load(joinpath("data",rda))[ "data." * dataset_name]
+        data = load(basename,joinpath("data",rda))[ "data." * dataset_name]
         return data 
     catch
         data = load(joinpath("data",rda))[dataset_name]
