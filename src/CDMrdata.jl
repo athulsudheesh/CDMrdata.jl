f144f23e58c99ef219e7dd03369e54b3f351e208
Athul Sudheesh <athuls0708@gmail.com>
2021-07-22 10:44:52 -0500

    testing path issue with opt level

--- src/CDMrdata.jl
+++ src/CDMrdata.jl
@@ -1,8 +1,11 @@
 module CDMrdata
-using CodecXz
-using CodecBzip2    
-using RData
-const module_path = @__DIR__
-    include("dataloaders.jl")
-    export load_data
-end
+    if isdefined(Base, :Experimental) && isdefined(Base.Experimental, Symbol("@optlevel"))
+        @eval Base.Experimental.@optlevel 1
+    end
+    using CodecXz
+    using CodecBzip2    
+    using RData
+    const module_path = @__DIR__
+        include("dataloaders.jl")
+        export load_data
+    end
